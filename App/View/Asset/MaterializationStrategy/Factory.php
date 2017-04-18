57b9061c85889b785d1d240226bed257d103742f
Michael Woodward <mikeymike.mw@gmail.com>
2017-04-18 09:57:28 +0100

    Only do changes in developer mode

--- App/View/Asset/MaterializationStrategy/Factory.php
+++ App/View/Asset/MaterializationStrategy/Factory.php
@@ -2,9 +2,11 @@
 
 namespace Jh\SymlinkAssetDeploys\App\View\Asset\MaterializationStrategy;
 
+use Magento\Framework\App\State;
 use Magento\Framework\App\View\Asset\MaterializationStrategy\Copy;
 use Magento\Framework\App\View\Asset\MaterializationStrategy\Factory as MaterializationStrategyFactory;
 use Magento\Framework\App\View\Asset\MaterializationStrategy\Symlink;
+use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\View\Asset;
 
 /**
@@ -15,8 +17,23 @@ class Factory extends MaterializationStrategyFactory
     const PREFERRED_STRATEGY = Symlink::class;
     const FALLBACK_STRATEGY  = Copy::class;
 
+    /**
+     * @var State
+     */
+    private $state;
+
+    public function __construct(State $state, ObjectManagerInterface $objectManager, $strategiesList = [])
+    {
+        $this->state = $state;
+        parent::__construct($objectManager, $strategiesList);
+    }
+
     public function create(Asset\LocalInterface $asset)
     {
+        if ($this->state->getMode() !== State::MODE_DEVELOPER) {
+            parent::create($asset);
+        }
+
         if (empty($this->strategiesList)) {
             $this->strategiesList[] = $this->objectManager->get(self::PREFERRED_STRATEGY);
             $this->strategiesList[] = $this->objectManager->get(self::FALLBACK_STRATEGY);
